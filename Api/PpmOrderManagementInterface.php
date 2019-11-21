d42c3ae2687927e559a1d6365d75422547c220ae
Ryan Stantz <ryanstantz@gmail.com>
2019-11-21 15:01:15 -0600

    Updates Ppm Order Management to handle preferred json object

--- Api/PpmOrderManagementInterface.php
+++ Api/PpmOrderManagementInterface.php
@@ -1,12 +1,13 @@
 <?php
 namespace Ppm\Fulfillment\Api;
 interface PpmOrderManagementInterface {
-  /**
-   * GET for Post api
-   * @param string $orderId
-   * @param mixed $tracks
-   * @return string
-   */
-  public function markAsShipped($orderId, $tracks);
+	/**
+	 * GET for Post api
+	 * @param string $orderId
+	 * @param string $TrackingNumber
+	 * @param string $Carrier
+	 * @param mixed $LineItems
+	 * @return string
+	 */
+	public function markAsShipped($orderId, $TrackingNumber, $Carrier, $LineItems);
 }
-?>
--- Model/PpmOrderManagement.php
+++ Model/PpmOrderManagement.php
@@ -1,65 +1,66 @@
 <?php
 namespace Ppm\Fulfillment\Model;
+// {
+//   “OrderId”: “string”,
+//   “TrackingNumber”: “string”,
+//   “Carrier”: “string”,
+//   “LineItems”: [
+//     { “ProductId”: “12345”, Quantity: 1, “LotNumber”: “12345”, “SerialNumber”: “ABCXYZ” },
+//     { “ProductId”: “56789”, Quantity: 45, “LotNumber”: “678”, “SerialNumber”: “” },
+//   ]
+// }
 class PpmOrderManagement {
-  /**
-   * {@inheritdoc}
-   */
-  public function markAsShipped($orderId, $tracks)
-  {
-    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-    $order = $objectManager->create('\Magento\Sales\Model\Order')->loadByAttribute('ppm_order_id', $orderId);
-    // Check if order can be shipped or has already shipped
-    if (! $order->canShip()) {
-      throw new \Magento\Framework\Exception\LocalizedException(
-        __('You can\'t create a shipment.')
-      );
-    }
-    // Initialize the order shipment object
-    $convertOrder = $objectManager->create('Magento\Sales\Model\Convert\Order');
-    $shipment = $convertOrder->toShipment($order);
-    // Loop through order items
-    foreach ($order->getAllItems() AS $orderItem) {
-      // Check if order item has qty to ship or is virtual
-      if (! $orderItem->getQtyToShip() || ! $orderItem->getProduct()->getFulfilledByPpm() || empty($orderItem->getProduct()->getPpmMerchantSku())) {
-        continue;
-      }
-      $qtyShipped = $orderItem->getQtyToShip();
-      // Create shipment item with qty
-      $shipmentItem = $convertOrder->itemToShipmentItem($orderItem)->setQty($qtyShipped);
-      // Add shipment item to shipment
-      $shipment->addItem($shipmentItem);
-    }
-    // Register shipment
-    $shipment->register();
-    $shipment->getOrder()->setIsInProcess(true);
-    try {
-      // Attempt to save multiple tracking numbers
-      if (!empty($tracks)) {
-        foreach ($tracks as $track) {
-          $data = array(
-            'carrier_code' => $track['carrier_code'],
-            'title' => $track['title'],
-            'track_number' => $track['track_number'],
-          );
-          $track = $objectManager->create('Magento\Sales\Model\Order\Shipment\TrackFactory')->create()->addData($data);
-          $shipment->addTrack($track);
-        }
-      }
-      // Save created shipment and order
-      $shipment->save();
-      $shipment->getOrder()->setPpmOrderStatus("shipped");
-      $shipment->getOrder()->save();
-      // Send email
-      // $objectManager->create('Magento\Shipping\Model\ShipmentNotifier')
-      //     ->notify($shipment);
-      $shipment->save();
-    } catch (\Exception $e) {
-      throw new \Magento\Framework\Exception\LocalizedException(
-        __($e->getMessage())
-      );
-    }
-    // return 'api GET return the $orderId ' . $orderId;
-    return $tracks;
-  }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function markAsShipped($orderId, $TrackingNumber, $Carrier, $LineItems)
+	{
+		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+		$order = $objectManager->create(‘\Magento\Sales\Model\Order’)->load($orderId);
+		// Check if order can be shipped or has already shipped
+		if (! $order->canShip()) {
+		    throw new \Magento\Framework\Exception\LocalizedException(
+		                    __(’You can\‘t create an shipment.’)
+		                );
+		}
+		// Initialize the order shipment object
+		$convertOrder = $objectManager->create(‘Magento\Sales\Model\Convert\Order’);
+		$shipment = $convertOrder->toShipment($order);
+		// Loop through order items
+		foreach ($order->getAllItems() AS $orderItem) {
+			foreach ($LineItems as $lineItem) {
+				if ($lineItem[‘ProductId’] == $orderItem->getProduct()->getPpmMerchantSku()) {
+					$qtyShipped = $lineItem[‘Quantity’];
+					$shipmentItem = $convertOrder->itemToShipmentItem($orderItem)->setQty($qtyShipped);
+					$shipment->addItem($shipmentItem);
+				}
+			}
+		}
+		// Register shipment
+		$shipment->register();
+		$shipment->getOrder()->setIsInProcess(true);
+		try {
+				// Attempt to save multiple tracking numbers
+		    $data = array(
+		      ‘carrier_code’ => ‘fedex’,
+		      ‘title’ => $Carrier,
+		      ‘track_number’ => $TrackingNumber,
+		    );
+				$track = $objectManager->create(‘Magento\Sales\Model\Order\Shipment\TrackFactory’)->create()->addData($data);
+		    $shipment->addTrack($track);
+		    // Save created shipment and order
+		    $shipment->save();
+				$shipment->getOrder()->setPpmOrderStatus(“shipped”);
+		    $shipment->getOrder()->save();
+		    // Send email
+		    // $objectManager->create(‘Magento\Shipping\Model\ShipmentNotifier’)
+		    //     ->notify($shipment);
+		    $shipment->save();
+		} catch (\Exception $e) {
+		    throw new \Magento\Framework\Exception\LocalizedException(
+		                    __($e->getMessage())
+		                );
+		}
+		return ‘api GET return the $orderId ’ . $orderId;
+	}
 }
-?>
